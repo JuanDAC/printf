b9c7e55b8973646017f15aaac0763e6f680ee2c1
JuanDAC <juanavila12.0308@gmail.com>
2021-03-13 21:52:17 +0000

    [init ~ evaluator.c] (firts lines evaluator)

--- evaluator.c
+++ evaluator.c
@@ -3,6 +3,21 @@
 
 
 
+void evaluator(token_t *tokens[], void *list_variables[])
+{
+	(void)tokens;
+	(void)list_variables;
+	int i;
+	token_t *current_token = NULL;
+
+	for (i = 0; tokens[i] != NULL; i++)
+	{
+		current_token = tokens[i];
+	}
+
+}
+
+
 
 
 
