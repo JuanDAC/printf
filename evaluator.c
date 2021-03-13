546dfbce7fe2aae59ed2e925c67cc4961e7e3120
JuanDAC <juanavila12.0308@gmail.com>
2021-03-13 23:27:46 +0000

    [update ~ evaluator.c] (new update prototype implementation)

--- evaluator.c
+++ evaluator.c
@@ -1,20 +1,42 @@
 #include "holberton.h"
 
 
-
-
-void evaluator(token_t *tokens[], void *list_variables[])
+char* (*handler_selector(token_t *token))(void *value)
 {
-	(void)tokens;
-	(void)list_variables;
 	int i;
-	token_t *current_token = NULL;
+	handler_link_t format_handlers_link[] = {
+		{integer, integer_handler},
+		{ocatal_integer, ocatal_integer_handler},
+		{NULL, NULL}
+	};
 
-	for (i = 0; tokens[i] != NULL; i++)
+	for (i = 0; format_handlers_link[i]->type != NULL; i++)
 	{
-		current_token = tokens[i];
+		if (token->type == format_handlers_link[i]->type)
+		{
+			return (format_handlers_link[i]->handler);
+		}
 	}
+}
 
+
+char **evaluator(token_t *tokens[], void *list_variables[], char *buffer_acumulator)
+{
+	int i, j = 0;
+
+	for (i = 0; tokens[i]->type != NULL; i++)
+	{
+		if (tokens[i].need_variable)
+		{
+			buffer_acumulator[i] = handler_selector(tokens[i])(list_variables[j]);
+			j++;
+		}
+		else
+		{
+			buffer_acumulator[i] = handler_selector(tokens[i])(tokens[i].literal);
+		}
+	}
+	return (buffer_acumulator);
 }
 
 
