7673be890f84aafe33b264d5464c94000583bd41
JuanDAC <juanavila12.0308@gmail.com>
2021-03-14 02:55:19 +0000

    [update ~ format_handlers.h] (add prototypes of mandatory format_handlers)

--- headers/format_handlers.h
+++ headers/format_handlers.h
@@ -2,5 +2,12 @@
 #define FORMAT_HANDLERS_H
 
 char *integer_handler(void *number);
+char *character(void *character);
+char *string(void *string);
+char *generic_integer(void *number);
+char *ocatal_integer(void *number);
+char *hexadecimal_integer(void *number);
+char *unsigned_integer(void *number);
+char *short_integer(void *number);
 
 #endif /* FORMAT_HANDLERS_H */
