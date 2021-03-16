3dd9cf27c89e63a0f5c4d31ede41a9d60fd1350d
jihuder <julioarenas85@gmail.com>
2021-03-16 17:11:55 +0000

    [update ~ format_handlers.c] (add percentage escape)

--- headers/format_handlers.h
+++ headers/format_handlers.h
@@ -19,12 +19,10 @@ char *string_handler(
 	const int attribute_length,
 	...
 );
-/* char *character_handler(const int attribute_length, ...);
-char *string_handler(const int attribute_length, ...);
-char *generic_integer_handler(const int attribute_length, ...);
-char *ocatal_integer_handler(const int attribute_length, ...);
-char *hexadecimal_integer_handler(const int attribute_length, ...);
-char *unsigned_integer_handler(const int attribute_length, ...);
-char *short_integer_handler(const int attribute_length, ...);
-*/
+char *percentage_escape_handler(
+	const garbage_collector_t *GC,
+	va_list list_variables,
+	const int attribute_length,
+	...
+);
 #endif /* FORMAT_HANDLERS_H */
--- headers/parser.h
+++ headers/parser.h
@@ -1,10 +1,19 @@
 #ifndef PARSE_H
 #define PARSE_H
 
+
+
 token_t **parser(
 	const garbage_collector_t *GC,
 	token_t **raw_tokens
 );
+
+#define SET_NORMAL_TOKEN(PT, PI, RT, RI)                   \
+	parse_tokens[parser_index] = raw_tokens[raw_index];\
+	parse_tokens[parser_index]->literal = str_copy(    \
+		raw_tokens[raw_index]->literal, 0);            \
+	parse_tokens[parser_index]->type = normal_string;  \
+	parser_index += 1;
 
 
 
