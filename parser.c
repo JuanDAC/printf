937a9cf02013a50dc205b17ee6746ed80da58f5e
jihuder <julioarenas85@gmail.com>
2021-03-16 18:56:17 +0000

    [update ~ format_handlers.h] (add prototypes of mandatory format_handlers)

--- parser.c
+++ parser.c
@@ -78,7 +78,6 @@ token_t **parser(const garbage_collector_t *GC, token_t **raw_tokens)
 					*raw_tokens[raw_index + shifting]->literal))
 				{
 					set_normal_token(parse_tokens, &parser_index, raw_tokens, raw_index);
-					SET_NORMAL_TOKEN(parse_tokens, parser_index, raw_tokens, raw_index);
 					break;
 				}
 		}
