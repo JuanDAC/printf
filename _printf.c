0ee2f8c1d76323b269613bf64c6ae0eb0c5e09bf
JuanDAC <juanavila12.0308@gmail.com>
2021-03-16 07:43:01 +0000

    [refactor ~ _printf.c] (conditionals)

--- _printf.c
+++ _printf.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * _printf -
- * @format:
- * Return:
+ * _printf - function to do formatted printing
+ * @format: text formating
+ * Return: number caracter printer or -1 if format is NULL or %
  */
 
 int _printf(const char *format, ...)
@@ -12,6 +12,8 @@ int _printf(const char *format, ...)
 	char *buffer;
 	va_list raw_argument_list;
 
+	if (NULL_OR_ONLY_PERCENTAGE(format))
+		return (-1);
 	va_start(raw_argument_list, format);
 	GC = malloc(sizeof(garbage_collector_t));
 
