40edcb00710db428dee57c666923bd9118004855
jihuder <julioarenas85@gmail.com>
2021-03-16 18:57:08 +0000

    [refctor ~ headers/format_handlers.h] (remove unnecessary prototype prototypes)

--- headers/parser.h
+++ headers/parser.h
@@ -6,9 +6,6 @@ token_t **parser(
 	token_t **raw_tokens
 );
 
-	PT[PI]->literal = str_copy(             \
-		RT[RI]->literal, 0);                \
-	PT[PI]->type = normal_string;           \
 	PI += 1;
 
 #endif /* PARSE_H */
