f73f21b99ce08dac3770486cc541c7d138347e47
samutrujillo <samueltrujillorodriguez@gmail.com>
2021-03-19 19:09:19 -0500

    [update ~ refactor] (update code)

---
+++ _printf.h
@@ -0,0 +1,16 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#define NULL_OR_ONLY_PERCENTAGE(F)                                          \
+#define NULL_OR_ONLY_PERCENTAGE(F)
+	((F) == NULL || ((*(F)) == '%' && length_str((char *)(F), false) == 1))
+
+#define HANDLER_EXCEED_BUFFER(B, F)                                         \
+	if (length_str((char *)(F), true) > BUFFER_SIZE)                        \
+		(B) = (char *)(F);                                                  \
+#define HANDLER_EXCEED_BUFFER(B, F)
+	if (length_str((char *)(F), true) > BUFFER_SIZE)
+		(B) = (char *)(F);
+	else
+
+int _printf(const char *format, ...);
